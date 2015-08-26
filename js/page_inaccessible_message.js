c1ec6810df7d4248d39e1fdce2c482b936301c35
qadan <daitken@discoverygarden.ca>
2015-08-26 12:41:11 -0300

    access rights customizations

---
+++ js/page_inaccessible_message.js
@@ -0,0 +1,11 @@
+(function ($) {
+  Drupal.behaviors.umkcdora_append_inaccessible_message = {
+    attach: function (context, settings) {
+      $("img.BRnoselect").one("error", function() {
+        if ( !$("#data-umkcdora-copyright-restricted").length ) {
+          $("#book-viewer").prepend('<div id="data-umkcdora-copyright-restricted" class="messages error">This material is restricted due to copyright and is unavailable for viewing online. If you would like a copy for personal research and study, please visit <a href="http://library.umkc.edu/spec-col-about/services">http://library.umkc.edu/spec-col-about/services</a>.</div>');
+        }
+      });
+    }
+  }
+})(jQuery);
