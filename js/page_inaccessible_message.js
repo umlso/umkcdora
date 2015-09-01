ef404ed0cb4baa142512426f57d02e946768ca0a
qadan <daitken@discoverygarden.ca>
2015-09-01 16:43:15 -0300

    fewer handlers, better comments

--- js/page_inaccessible_message.js
+++ js/page_inaccessible_message.js
@@ -1,8 +1,10 @@
 (function ($) {
   Drupal.behaviors.umkcdora_append_inaccessible_message = {
     attach: function (context, settings) {
+      // Attach error event handlers s to all new img.BRnoselect elements, right
+      // up until the point an error event is fired from one of them.
       window.IslandoraBookReader.prototype.drawLeafs = function() {
-        // In the case where images could not be loaded, append an error message.
+        // Standard drawLeafs functionality.
         if (1 == this.mode) {
           this.drawLeafsOnePage();
         } else if (3 == this.mode) {
@@ -10,11 +12,14 @@
         } else {
           this.drawLeafsTwoPage();
         }
-        $('img.BRnoselect').one('error', function() {
-          if ( !$("#data-umkcdora-copyright-restricted").length ) {
-            $("#book-viewer").prepend('<div id="data-umkcdora-copyright-restricted" class="messages error">This material is restricted due to copyright and is unavailable for viewing online. If you would like a copy for personal research and study, please visit <a href="http://library.umkc.edu/spec-col-about/services">http://library.umkc.edu/spec-col-about/services</a>.</div>');
-          }
-        });
+        // Attach error handlers to the new images.
+        if ( !$("#data-umkcdora-copyright-restricted").length ) {
+          $('img.BRnoselect').one('error', function() {
+            if ( !$("#data-umkcdora-copyright-restricted").length ) {
+              $("#book-viewer").prepend('<div id="data-umkcdora-copyright-restricted" class="messages error">This material is restricted due to copyright and is unavailable for viewing online. If you would like a copy for personal research and study, please visit <a href="http://library.umkc.edu/spec-col-about/services">http://library.umkc.edu/spec-col-about/services</a>.</div>');
+            }
+          });
+        }
       }
     }
   }
